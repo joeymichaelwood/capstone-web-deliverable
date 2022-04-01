3a79a4065c8e5bd6c9e6ecb9e434c00b28de7718
Joey Wood <josephwood@ischool.berkeley.edu>
2022-04-01 14:20:55 -0700

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+
+const HeaderImageBoundingBox = styled.div`
+  height: 70vh;
+  width: 100vw;
+`
+
+const HeaderImage = styled.img`
+  height: 100%;
+  width: 100%;
+`
+
+const GroupMembers = styled.p`
+  font-size: calc(10px + 3vmin);
+`
 
 function App() {
   return (
     <div className="App">
+      <HeaderImageBoundingBox>
+        <HeaderImage src={"../starfish-boxes.png"}></HeaderImage>
+      </HeaderImageBoundingBox>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        W207 Capstone
+        <br/>
+        Crown of Thorns Starfish
+        <GroupMembers>
+          by Alan Zhang, Frank Tank, Jocelyn Lu, Joseph Wood, and Spencer Hong
+        </GroupMembers>
       </header>
+
     </div>
   );
 }
